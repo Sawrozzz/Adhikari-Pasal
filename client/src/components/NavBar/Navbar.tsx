c0245f85fb2711eb70369e8becf5c19093c6631c
Saroj Adhikari <sawroz.adhikari@gmail.com>
2025-01-11 17:55:59 +0545

    Role based navbar displayed completed

--- client/src/components/NavBar/Navbar.tsx
+++ client/src/components/NavBar/Navbar.tsx
@@ -1,6 +1,10 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import useAuthStore from "../../store/authStore";
 
 const Navbar = () => {
+  const { user, isLoggedIn, logout } = useAuthStore();
+
   return (
     <nav>
       <ul className="flex gap-10 text-white">
@@ -10,12 +14,34 @@ const Navbar = () => {
         <li className="cursor-pointer hover:text-gray-900">
           <Link to="/cart">Cart</Link>
         </li>
-        <li className="cursor-pointer hover:text-gray-900">
-          <Link to="/login">Login</Link>
-        </li>
-        <li className="cursor-pointer hover:text-gray-900">
-          <Link to="/signup">Signup</Link>
-        </li>
+
+        {!isLoggedIn ? (
+          <>
+            <li className="cursor-pointer hover:text-gray-900">
+              <Link to="/login">Login</Link>
+            </li>
+            <li className="cursor-pointer hover:text-gray-900">
+              <Link to="/signup">Signup</Link>
+            </li>
+          </>
+        ) : (
+          <>
+            {/* Show extra links for Admin users */}
+            {user?.role === "ADMIN" && (
+              <>
+                <li className="cursor-pointer hover:text-gray-900">
+                  <Link to="/add-product">Add Products</Link>
+                </li>
+                <li className="cursor-pointer hover:text-gray-900">
+                  <Link to="/view-users">View Users</Link>
+                </li>
+              </>
+            )}
+            <li className="cursor-pointer hover:text-gray-900" onClick={logout}>
+              Logout
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
