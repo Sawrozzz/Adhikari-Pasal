bc26592f5304ed0926d86f8fddf7a375952f81b7
Saroj Adhikari <sawroz.adhikari@gmail.com>
2025-01-11 13:49:28 +0545

    logout code

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -111,3 +111,20 @@ export const login = async (req, res) => {
     });
   }
 };
+
+//code for logout user
+export const logout = (req, res) => {
+  try {
+    res.clearCookie("token");
+    res.status(200).json({
+      message: "Logout successful",
+      success: true,
+    });
+  } catch (error) {
+    console.log("Error during logout", error.message);
+    res.status(500).json({
+      message: "Server Error",
+      success: false,
+    });
+  }
+};
