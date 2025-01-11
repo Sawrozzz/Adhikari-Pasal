915c5ecac4bbaff8df7f6c0c5019f61595242a16
Saroj Adhikari <sawroz.adhikari@gmail.com>
2025-01-11 13:38:54 +0545

    login route added

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,13 +1,15 @@
 import express from "express";
 const router = express.Router();
 
-import { register } from "../controllers/authController.js";
+import { login, register } from "../controllers/authController.js";
 
 router.get("/",(req, res)=>{
 res.send("Working fine on user route")
 });
 
 router.post("/register", register);
+
+router.post("/login", login)
 
 
 export default router;
