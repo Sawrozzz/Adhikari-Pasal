76e493c72967a03ae8de91fd91b8d6fcaaa53647
Saroj Adhikari <sawroz.adhikari@gmail.com>
2025-01-11 10:09:17 +0545

    added home page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import Navbar from "./components/NavBar/Navbar";
 import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
 import Cart from "./pages/Cart/Cart";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Header />
       <Navbar />
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
