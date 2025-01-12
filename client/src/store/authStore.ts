00a4e579b4bf5c9e6e1b982420553a2ca6eac550
Saroj Adhikari <sawroz.adhikari@gmail.com>
2025-01-12 11:13:05 +0545

    store file to handle All users and profile of a user

--- client/src/store/authStore.ts
+++ client/src/store/authStore.ts
@@ -1,18 +1,49 @@
 import { create } from "zustand";
 import axios from "axios";
 
-const baseURL = "http://localhost:5000";
+const baseURL = "http://localhost:5000/users";
 
 const useAuthStore = create((set) => ({
   user: JSON.parse(localStorage.getItem("user") || "null"),
   token: localStorage.getItem("token"),
-  isLoggedIn: localStorage.getItem("token") ? true : false,
+  // isLoggedIn: localStorage.getItem("token") ? true : false,
+  isLoggedIn: !!localStorage.getItem("token"),
+  allUsers: [],
+
+   userProfile: async () => {
+    const user = JSON.parse(localStorage.getItem("user") || "null");
+    const token = localStorage.getItem("token");
+
+    if (!user || !token) {
+      console.error("User is not logged in.");
+      
+      return;
+    }
+
+    // try {
+    //   const response = await axios.get(`${baseURL}/profile/${user.id}`, {
+    //     headers: {
+    //       Authorization: `Bearer ${token}`,
+    //     },
+    //   });
+
+    //   if (response.status === 200) {
+    //     console.log("Profile fetched successfully:", response.data);
+    //     set({ user: response.data }); // Update Zustand store with fetched user profile
+    //   } else {
+    //     throw new Error("Failed to fetch user profile.");
+    //   }
+    // } catch (error) {
+    //   console.error("Error while fetching user profile:", error);
+    //   alert("Failed to fetch user profile.");
+    // }
+  },
 
   signup: async (userData) => {
     console.log("clicked again");
 
     try {
-      const response = await axios.post(`${baseURL}/users/register`, userData);
+      const response = await axios.post(`${baseURL}/register`, userData);
       const { userData: registeredData } = response.data;
       console.log(response.data.userData);
 
@@ -29,7 +60,7 @@ const useAuthStore = create((set) => ({
 
   login: async (userData) => {
     try {
-      const response = await axios.post(`${baseURL}/users/login`, userData);
+      const response = await axios.post(`${baseURL}/login`, userData);
       const { token, userData: loggedInUserData } = response.data;
       set({
         user: loggedInUserData,
@@ -53,6 +84,21 @@ const useAuthStore = create((set) => ({
 
     // Remove token from localStorage
     localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    console.log("User logged out successfully.");
+  },
+
+  fetchUsers: async () => {
+    try {
+      const response = await axios.get(`${baseURL}/allUsers`);
+
+      set({
+        allUsers: response.data.data,
+      });
+    } catch (error) {
+      console.error("Error while fetching users:", error);
+      alert(error);
+    }
   },
 }));
 
