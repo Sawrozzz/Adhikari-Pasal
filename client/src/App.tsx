c5ecbb1a602c4a6da18738afcd929a1e9be14c2f
Saroj Adhikari <sawroz.adhikari@gmail.com>
2025-01-12 11:12:03 +0545

    refactor some changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,8 @@ import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
 import Cart from "./pages/Cart/Cart";
 import Home from "./pages/Home/Home";
+import AllUsers from "./pages/AllUsers/AllUsers";
+import Profile from "./pages/Profile/Profile";
 
 function App() {
   return (
@@ -17,6 +19,8 @@ function App() {
         <Route path="/cart" element={<Cart />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/all-users" element={<AllUsers />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
