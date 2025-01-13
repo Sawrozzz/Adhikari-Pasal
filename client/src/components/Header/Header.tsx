b98dc9a665eb7a9838aa0d99930e6e40128a10e3
Saroj Adhikari <sawroz.adhikari@gmail.com>
2025-01-13 22:34:29 +0545

    added search bar in header

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
 import Navbar from "../NavBar/Navbar";
+import SearchBar from "../SearchBar/SearchBar";
 const Header = () => {
   return (
-    <div className="flex justify-around bg-gray-400 p-4">
+    <div className="flex justify-between bg-gray-400 p-4">
       <div className="hover:cursor-pointer">
         <Link to="/">Adhikari Pasal</Link>
       </div>
+      <SearchBar />
       <Navbar />
+
     </div>
   );
 };
