acb65350d5e00e8fd82167c6e5870373fc9eb770
Saroj Adhikari <sawroz.adhikari@gmail.com>
2025-01-13 22:38:10 +0545

    get all product route complete

--- server/routes/productRouter.js
+++ server/routes/productRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
 import upload from "../middleware/multer.js";
-import { create } from "../controllers/productController.js";
+import { allProducts, create } from "../controllers/productController.js";
 
 const router = express.Router();
 
 
 router.post("/create",  upload.single("image"),create);
+router.get("/all-products",allProducts)
 export default router;
