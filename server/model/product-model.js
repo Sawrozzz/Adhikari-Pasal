c6beab115707e6f17dc3b1268fb7522e956be80b
Saroj Adhikari <sawroz.adhikari@gmail.com>
2025-01-13 22:37:51 +0545

    few changes on product model

--- server/model/product-model.js
+++ server/model/product-model.js
@@ -1,15 +1,16 @@
 import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
-    name:String,
-    price:Number,
-    description:String,
-    discount:{
-     type:Number,
-     default:0
-    },
-    image:String,
-})
+  name: String,
+  price: Number,
+  description: String,
+  discountedPrice: Number,
+  discount: {
+    type: Number,
+    default: 0,
+  },
+  image: String,
+});
 
 const Product = mongoose.model("product", productSchema);
 export default Product;
