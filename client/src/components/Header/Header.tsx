f8f68f49711967d5b469dedb663742ee2cf7fead
Saroj Adhikari <sawroz.adhikari@gmail.com>
2025-01-13 09:43:02 +0545

    some changes

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,8 +1,11 @@
+import { Link } from "react-router-dom";
 import Navbar from "../NavBar/Navbar";
 const Header = () => {
   return (
     <div className="flex justify-around bg-gray-400 p-4">
-      <div className="hover:cursor-pointer">Logo</div>
+      <div className="hover:cursor-pointer">
+        <Link to="/">Adhikari Pasal</Link>
+      </div>
       <Navbar />
     </div>
   );
--- client/src/components/NavBar/Navbar.tsx
+++ client/src/components/NavBar/Navbar.tsx
@@ -14,19 +14,19 @@ const Navbar = () => {
   return (
     <nav>
       <ul className="flex gap-10 text-white">
-        <li className="cursor-pointer hover:text-gray-900">
+        <li className="cursor-pointer hover:text-blue-500">
           <Link to="/">Home</Link>
         </li>
-        <li className="cursor-pointer hover:text-gray-900">
+        <li className="cursor-pointer hover:text-blue-500">
           <Link to="/cart">Cart</Link>
         </li>
 
         {!isLoggedIn ? (
           <>
-            <li className="cursor-pointer hover:text-gray-900">
+            <li className="cursor-pointer hover:text-blue-500">
               <Link to="/login">Login</Link>
             </li>
-            <li className="cursor-pointer hover:text-gray-900">
+            <li className="cursor-pointer hover:text-blue-500">
               <Link to="/signup">Signup</Link>
             </li>
           </>
@@ -35,18 +35,21 @@ const Navbar = () => {
             {/* Show extra links for Admin users */}
             {user?.role === "ADMIN" && (
               <>
-                <li className="cursor-pointer hover:text-gray-900">
+                <li className="cursor-pointer hover:text-blue-500">
                   <Link to="/add-product">Add Products</Link>
                 </li>
-                <li className="cursor-pointer hover:text-gray-900">
+                <li className="cursor-pointer hover:text-blue-500">
                   <Link to="/all-users">View Users</Link>
                 </li>
               </>
             )}
-            <li className="cursor-pointer hover:text-gray-900">
-             <Link to="/profile">Profile</Link>
+            <li className="cursor-pointer hover:text-blue-500">
+              <Link to="/profile">Profile</Link>
             </li>
-            <li className="cursor-pointer hover:text-gray-900" onClick={handleLogout}>
+            <li
+              className="cursor-pointer hover:text-blue-500"
+              onClick={handleLogout}
+            >
               Logout
             </li>
           </>
