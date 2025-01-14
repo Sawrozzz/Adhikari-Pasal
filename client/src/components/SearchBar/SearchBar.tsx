fd1c68db3f49b0fea401577d4fd1dbbf5b52eb7f
Saroj Adhikari <sawroz.adhikari@gmail.com>
2025-01-14 19:00:22 +0545

    search bar complete

--- client/src/components/SearchBar/SearchBar.tsx
+++ client/src/components/SearchBar/SearchBar.tsx
@@ -1,7 +1,22 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import useProductStore from "../../store/productStore";
+
 
 const SearchBar = () => {
      const [query, setQuery] = useState("");
+     const {searchProducts} = useProductStore();
+     const navigate = useNavigate();
+   
+     const handleSearch = async(e) =>{
+      e.preventDefault();
+      if(query.trim()){
+        await searchProducts(query.trim()); // Trigger the search in the Zustand store
+        navigate(`/product/search?category=${query.trim()}`)
+      }
+     }
+
        const handleClear = () => {
          setQuery("");
        };
@@ -15,7 +30,7 @@ const SearchBar = () => {
         className="flex-1 bg-transparent outline-none px-4 py-2 text-gray-800"
       />
 
-      {query &&  (
+      {query && (
         <button
           onClick={handleClear}
           className="text-gray-500 hover:text-gray-800 px-2"
@@ -24,7 +39,10 @@ const SearchBar = () => {
         </button>
       )}
 
-      <button className="bg-purple-600 text-white px-4 py-2 rounded-full hover:bg-purple-700">
+      <button
+        onClick={handleSearch}
+        className="bg-purple-600 text-white px-4 py-2 rounded-full hover:bg-purple-700"
+      >
         Search
       </button>
     </div>
