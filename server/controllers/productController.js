30dc98c293829b92521ea3acc2e446bf91996a43
Saroj Adhikari <sawroz.adhikari@gmail.com>
2025-01-14 19:02:59 +0545

    search product logic complete

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -2,64 +2,95 @@ import Product from "../model/product-model.js";
 
 //code to create product
 export const create = async (req, res) => {
-  const { name, price,category, description, discount } = req.body;
+  const { name, price, category, description, discount } = req.body;
 
   const defaultImage =
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQinI_44p5jN05YioLyPBhn_1j5tsl7q85rfA&s";
 
+  try {
+    const actualImage = req.file ? req.file.path : defaultImage;
+    const discountedPrice = discount ? price - (price * discount) / 100 : price;
 
-    
-    try {
-      const actualImage = req.file ? req.file.path : defaultImage;
-      const discountedPrice = discount
-      ? price - (price * discount) / 100
-      : price; 
+    const newProduct = await Product.create({
+      name,
+      category,
 
-      const newProduct = await Product.create({
-        name,
-        category,
-        
-        price: Number(price),
-        discountedPrice:Number(discountedPrice),
-        description,
-        discount: Number(discount),
-        image: actualImage,
-      });
+      price: Number(price),
+      discountedPrice: Number(discountedPrice),
+      description,
+      discount: Number(discount),
+      image: actualImage,
+    });
 
-      const savedProduct = await newProduct.save();
+    const savedProduct = await newProduct.save();
 
-      res.status(201).json({
-        message: "Product created successfully",
-        product: savedProduct,
-        success: true,
-      });
-    } catch (error) {
+    res.status(201).json({
+      message: "Product created successfully",
+      product: savedProduct,
+      success: true,
+    });
+  } catch (error) {
     console.error("Error while creating product:", error.message);
-    res.status(500).json({ message: "Failed to create product", error ,success:false});
+    res
+      .status(500)
+      .json({ message: "Failed to create product", error, success: false });
   }
 };
 
 //code to get all products
-export const allProducts = async(req, res) =>{
- try{
-  const products = await Product.find();
-  // if(!products.length){
-  //   return res.status(400).json({
-  //     message:"No products found",
-  //     success:false,
-  //   });
-  // }
-  res.status(200).json({
-    message:"Products retrieved successfully",
-    success:true,
-    data:products,
-  })
+export const allProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    // if(!products.length){
+    //   return res.status(400).json({
+    //     message:"No products found",
+    //     success:false,
+    //   });
+    // }
+    res.status(200).json({
+      message: "Products retrieved successfully",
+      success: true,
+      data: products,
+    });
+  } catch (error) {
+    console.log("Error during getting all products", error.message);
+    res.status(500).json({
+      message: "Server Error",
+      success: false,
+    });
+  }
+};
 
- }catch(error){
-  console.log("Error during getting all products", error.message);
-  res.status(500).json({
-    message:"Server Error",
-    success:false
-  })
- } 
-}
+export const searchProducts = async (req, res) => {
+  try {
+       const { category } = req.query;
+
+      const query = category
+        ? { category: { $regex: category, $options: "i" } } // Case-insensitive search
+        : {};
+
+      const products = await Product.find(query);
+
+         if(!products.length){
+             return res.status(404).json({
+               message: "No products found for the specified category",
+               success: false,
+             });
+         }                        
+
+    res.status(200).json({
+      message: "Products found successfully",
+      success: true,
+      data:products,
+    });
+  } catch (error) {
+    console.error(
+      "Error during searching products with this category:",
+      error.message
+    );
+    res.status(500).json({
+      message: "Server Error",
+      success: false,
+    });
+  }
+};
