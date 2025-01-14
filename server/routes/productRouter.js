794659679310127836891e807f8ddec509270085
Saroj Adhikari <sawroz.adhikari@gmail.com>
2025-01-14 19:03:48 +0545

    new route for search product

--- server/routes/productRouter.js
+++ server/routes/productRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
 import upload from "../middleware/multer.js";
-import { allProducts, create } from "../controllers/productController.js";
+import { allProducts, create, searchProducts } from "../controllers/productController.js";
 
 const router = express.Router();
 
 
 router.post("/create",  upload.single("image"),create);
-router.get("/all-products",allProducts)
+router.get("/all-products",allProducts);
+router.get("/search", searchProducts);
 export default router;
