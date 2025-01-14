1eda97c02d602a329da92dc4553a77f585240814
Saroj Adhikari <sawroz.adhikari@gmail.com>
2025-01-14 19:03:31 +0545

    image with .webp format support now

--- server/middleware/multer.js
+++ server/middleware/multer.js
@@ -3,12 +3,12 @@ import {CloudinaryStorage} from 'multer-storage-cloudinary';
 import cloudinary from '../config/cloudinary.js';
 
 const storage = new CloudinaryStorage({
-    cloudinary,
-    params:{
-        folder:"products",
-        allowed_formats:["jpg", "png","jpeg"],
-    },
-})
+  cloudinary,
+  params: {
+    folder: "products",
+    allowed_formats: ["jpg", "png", "jpeg", "webp"],
+  },
+});
 
 const upload = multer({storage});
 
