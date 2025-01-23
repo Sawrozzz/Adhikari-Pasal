ab6140b972006229e0a6b9c0debb6f1e251a50be
Saroj Adhikari <sawroz.adhikari@gmail.com>
2025-01-23 14:55:05 +0545

    change model to add the carts in user table

--- server/model/user-model.js
+++ server/model/user-model.js
@@ -1,18 +1,27 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
- const userSchema = new mongoose.Schema({
-    fullName:String,
-    email:String,
-    password:String,
-    address:String,
-    phone:Number,
-    picture:String,
-    role:{
-        type:String,
-        enum:['ADMIN','NORMAL'],
-        default:'NORMAL',
-    }
-    
-})
-const User = mongoose.model("user",userSchema);
+const userSchema = new mongoose.Schema({
+  fullName: String,
+  email: String,
+  password: String,
+  address: String,
+  phone: Number,
+  picture: String,
+  role: {
+    type: String,
+    enum: ["ADMIN", "NORMAL"],
+    default: "NORMAL",
+  },
+  cart: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "product",
+    },
+  ],
+  orders: {
+    type: Array,
+    default: [],
+  },
+});
+const User = mongoose.model("user", userSchema);
 export default User;
