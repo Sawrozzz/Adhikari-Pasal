ff4254cb7cbd1a51d26a34485b3be05043639cf0
Saroj Adhikari <sawroz.adhikari@gmail.com>
2025-01-23 17:22:03 +0545

    delete cart route complete

--- server/routes/cartRouter.js
+++ server/routes/cartRouter.js
@@ -1,10 +1,12 @@
 import express from "express";
 const router = express.Router();
-import { addToCart, displayCart } from "../controllers/cartController.js";
+import { addToCart, displayCart, deleteCart } from "../controllers/cartController.js";
 import { isloggedIn } from "../middleware/isLoggedIn.js";
 
 router.post("/add-to-cart/:id",addToCart);
 
 router.get("/my-carts",displayCart)
+
+router.delete("/delete-cart",deleteCart)
 
 export default router;
