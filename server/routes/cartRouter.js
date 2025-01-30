90222d627ef6a52ac1bae05456bd5662540ef1db
Saroj Adhikari <sawroz.adhikari@gmail.com>
2025-01-30 14:57:02 +0545

    route for updating cart complete

--- server/routes/cartRouter.js
+++ server/routes/cartRouter.js
@@ -1,12 +1,19 @@
 import express from "express";
 const router = express.Router();
-import { addToCart, displayCart, deleteCart } from "../controllers/cartController.js";
+import {
+  addToCart,
+  displayCart,
+  deleteCart,
+  updateCart,
+} from "../controllers/cartController.js";
 import { isloggedIn } from "../middleware/isLoggedIn.js";
 
-router.post("/add-to-cart/:id",addToCart);
+router.post("/add-to-cart/:id", addToCart);
 
-router.get("/my-carts",displayCart)
+router.get("/my-carts", displayCart);
 
-router.delete("/delete-cart/:id",deleteCart)
+router.delete("/delete-cart/:id", deleteCart); 
+
+router.patch("/update-cart/:id", updateCart);
 
 export default router;
