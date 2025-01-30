25e306d20ae92d992ac881a1fa136fc67f82d377
Saroj Adhikari <sawroz.adhikari@gmail.com>
2025-01-30 14:55:40 +0545

    almost all the stuffs for cart is completed

--- client/src/pages/Cart/Cart.jsx
+++ client/src/pages/Cart/Cart.jsx
@@ -4,12 +4,40 @@ import useAuthStore from "../../store/authStore";
 import { useNavigate } from "react-router-dom";
 
 const Cart = () => {
-  const { cart, displayCart, cartCount, removeCart, loading } = useCartStore();
+  const {
+    cart,
+    displayCart,
+    cartCount,
+    removeCart,
+    loading,
+    updateCartQuantity,
+  } = useCartStore();
   const { user } = useAuthStore();
   const navigate = useNavigate();
   useEffect(() => {
-    displayCart(user.email);
-  }, [user]);
+    if (user?.email) {
+      displayCart(user.email);
+    }
+  }, [user, displayCart]);
+
+  //increase quantity
+    const handleIncreaseQuantity = (
+      cartId,
+      currentQuantity,
+    ) => {
+      const newQuantity = currentQuantity + 1;
+      updateCartQuantity(user.email, cartId, newQuantity);
+    };
+
+    //decrease quantity
+
+    const handleDecreaseQuantity = (
+      cartId,
+      currentQuantity,
+    ) => {
+      const newQuantity = currentQuantity > 1 ? currentQuantity - 1 : 1;
+      updateCartQuantity(user.email, cartId, newQuantity);
+    };
 
   //to delete cart
   const handleRemoveCart = async (productId) => {
@@ -28,17 +56,16 @@ const Cart = () => {
     // alert("cart deleted successfully")
   };
 
-  const calculateTotalAmount = () => {
-    return cart.reduce((total, item) => total + item.price * item.quantity, 0);
-  };
-
   return (
     <div className="flex flex-col sm:flex-row justify-between px-2">
       <section className="flex flex-col gap-8 px-2 w-[60%] h-[45rem] overflow-y-scroll scrollbar-thin scrollbar-thumb-rounded scrollbar-thumb-gray-900 scrollbar-track-gray-100">
         <h2 className="text-2xl font-mono">Total Cart: {cartCount}</h2>
         <div className="flex flex-col gap-4 sm:flex-row flex-wrap ">
           {cart.map((item, index) => (
-            <div key={index} className="lg:w-[30%] rounded-md overflow-hidden">
+            <div
+              key={index}
+              className="lg:w-[30%] rounded-md overflow-hidden bg-slate-200"
+            >
               <div className="flex justify-center items-center">
                 <img
                   className="w-36 h-36 object-cover rounded"
@@ -47,10 +74,36 @@ const Cart = () => {
                 />
               </div>
               <h3 className="text-xl text-black text-center">{item.name}</h3>
-              <div className="flex items-center justify-between px-5 py-3 bg-purple-500">
+              <div className="flex flex-col px-5 py-3 bg-purple-500">
                 <h4 className="text-lg">Price: {item.price}</h4>
                 <h2>Discounted Price: {item.discountedPrice}</h2>
-                <h4>Quantity: {item.quantity}</h4>
+                <h4 className="flex justify-between mb-2">
+                  <span>Quantity: {item.quantity}</span>
+                  <span
+                    onClick={() =>
+                      handleIncreaseQuantity(
+                        item._id,
+                        item.quantity,
+                    
+                      )
+                    }
+                    className=" border border-black w-8 h-8 text-center rounded-full text-xl cursor-pointer hover:bg-purple-800"
+                  >
+                    +
+                  </span>
+                  <span
+                    onClick={() =>
+                      handleDecreaseQuantity(
+                        item._id,
+                        item.quantity,
+                  
+                      )
+                    }
+                    className=" border border-black w-8 h-8 text-center rounded-full text-xl cursor-pointer hover:bg-purple-800"
+                  >
+                    -
+                  </span>
+                </h4>
                 <button
                   onClick={() => handleRemoveCart(item._id)}
                   disabled={loading}
@@ -67,12 +120,19 @@ const Cart = () => {
         <div class="border-l-2 border-black  h-[100%]"></div>
       </div>
       <section className="flex flex-col w-[35%]">
-        <h2>Price Breakdown</h2>
-        <div class="flex mt-5">
-          <h3 class="w-1/3 text-xl">Total Amount</h3>
-          <h3 class="font-semibold text-xl text-green-700">Rs 3000</h3>
+        <div class="flex mt-5 mb-7">
+          <h3 class="w-2/4 text-2xl">Total Amount to be Paid:</h3>
+          <h3 class="font-semibold text-2xl text-green-700">
+            Rs{" "}
+            {cart.reduce(
+              (total, item) => total + item.discountedPrice * item.quantity,
+              0
+            )}
+          </h3>
         </div>
-        <h3>Buy Now</h3>
+        <h3 className="border w-1/3 h-10 bg-green-500 flex justify-center items-center rounded hover:bg-green-600 cursor-pointer">
+          Buy & Chekout{" "}
+        </h3>
       </section>
     </div>
   );
