b22632f331acb9aefff66a2a61df2d44d38019e0
Saroj Adhikari <sawroz.adhikari@gmail.com>
2025-03-29 23:08:45 +0545

    user and product count coming dynamically

--- client/src/pages/AdminDashboard/Dashboard.tsx
+++ client/src/pages/AdminDashboard/Dashboard.tsx
@@ -1,26 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Pie } from "react-chartjs-2";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import Calendar from "react-calendar";
 import 'react-calendar/dist/Calendar.css'
 
+import useAuthStore from "../../store/authStore";
+import useProductStore from "../../store/productStore";
+
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 const Dashboard = () => {
   const [data] = useState({
-    users: 120,
-    orders: 80,
-    products: 50,
+    orders: 8,
     revenue: 50000,
   });
 
+   const { allUsersCount, fetchUsers } = useAuthStore();
+   const { productLength, fetchAllProducts } = useProductStore();
+   useEffect(() => {
+     fetchUsers();
+     fetchAllProducts();
+   }, [fetchUsers, fetchAllProducts]);
   const pieData = {
     labels: ["Users", "Orders", "Products"],
     datasets: [
       {
-        data: [data.users, data.orders, data.products],
-        backgroundColor: ["#ff6384", "#36a2eb", "#ffce56"],
-        hoverBackgroundColor: ["#ff4d6a", "#2a91d8", "#ffb733"],
+        data: [allUsersCount, data.orders, productLength],
+        backgroundColor: ["#0F60F7", "#47f30e", "#ffce56"],
+        hoverBackgroundColor: ["#3072ec", "#84ff5b", "#ffb733"],
       },
     ],
   };
@@ -30,13 +37,15 @@ const Dashboard = () => {
     maintainAspectRatio: false, // Allow custom width/height
   };
 
+  
+
   return (
     <div className="flex flex-col mt-5 md:flex-row h-screen w-full">
       <main className=" flex  flex-col  items-center w-full p-5">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-5 mb-5 w-8/12">
           <div className="bg-blue-500 text-white p-4 rounded-lg">
             <h3 className="text-lg">Total Users</h3>
-            <p className="text-2xl font-bold">{data.users}</p>
+            <p className="text-2xl font-bold">{allUsersCount}</p>
           </div>
           <div className="bg-green-500 text-white p-4 rounded-lg">
             <h3 className="text-lg">Total Orders</h3>
@@ -44,7 +53,7 @@ const Dashboard = () => {
           </div>
           <div className="bg-yellow-500 text-white p-4 rounded-lg">
             <h3 className="text-lg">Total Products</h3>
-            <p className="text-2xl font-bold">{data.products}</p>
+            <p className="text-2xl font-bold">{productLength}</p>
           </div>
           <div className="bg-red-500 text-white p-4 rounded-lg">
             <h3 className="text-lg">Total Revenue</h3>
@@ -74,5 +83,6 @@ const Dashboard = () => {
     </div>
   );
 };
+
 
 export default Dashboard;
