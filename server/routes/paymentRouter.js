3685b06ccc02d43c99c6dcfb66a550b76d1f2f1b
Saroj Adhikari <sawroz.adhikari@gmail.com>
2025-03-29 23:06:02 +0545

    order is created while payment init

--- server/routes/paymentRouter.js
+++ server/routes/paymentRouter.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import { initializeKhalti, verifyPayment } from '../controllers/paymentController.js';
-
+import { isloggedIn } from '../middleware/isLoggedIn.js';
 const router = express.Router();
 
-router.post("/initialize-khalti", initializeKhalti);
+router.post("/initialize-khalti",isloggedIn, initializeKhalti);
 
 router.get("/complete-khalti-payment", verifyPayment);
 
