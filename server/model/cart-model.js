94ff68ecbe8382995a283d73f66c86b688468d0e
Saroj Adhikari <sawroz.adhikari@gmail.com>
2025-03-29 23:06:20 +0545

    few changes on models

--- server/model/cart-model.js
+++ server/model/cart-model.js
@@ -1,25 +1,33 @@
 import mongoose from "mongoose";
 
-const cartSchema = new mongoose.Schema({
-  product: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "product",
-    required: true,
+const cartSchema = new mongoose.Schema(
+  {
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "product",
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: false,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      default: 1,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    discountedPrice: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
   },
-  quantity: {
-    type: Number,
-    required: true,
-    default: 1,
-  },
-  price: {
-    type: Number,
-    required: true,
-  },
-  discountedPrice: {
-    type: Number,
-    required: true,
-    default:0,
-  },
-},{timestamps:true});
+  { timestamps: true }
+);
 
-export const cartItem = mongoose.model("cartItem",cartSchema);
+export const cartItem = mongoose.model("cartItem", cartSchema);
